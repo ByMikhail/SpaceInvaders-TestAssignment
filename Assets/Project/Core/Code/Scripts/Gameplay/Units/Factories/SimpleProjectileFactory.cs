fa30ce946c7f8712f31af24c0d0b5bec0838c208
Mikhail Bychkov <mikbychkov@gmail.com>
2023-06-06 02:15:27 +0700

    Implement simple projectile factory

---
+++ Assets/Project/Core/Code/Scripts/Gameplay/Units/Factories/SimpleProjectileFactory.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace SpaceInvaders.Gameplay.Units.Factories
+{
+    [CreateAssetMenu(menuName = "Gameplay/Factories/SimpleProjectileFactory", fileName = "SimpleProjectileFactory")]
+    public class SimpleProjectileFactory : ProjectileFactoryBase
+    {
+        [SerializeField] private Projectile _projectilePrefab;
+
+        public override Projectile Create(string ignoreTag, Vector3 position, Quaternion direction)
+        {
+            var projectile = Instantiate(_projectilePrefab, position, direction);
+            projectile.Init(ignoreTag);
+
+            return projectile;
+        }
+    }
+}
