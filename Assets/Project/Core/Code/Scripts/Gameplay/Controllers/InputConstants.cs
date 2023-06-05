d30d89ca6dc6b567fb19d768f36b498e6860b049
Mikhail Bychkov <mikbychkov@gmail.com>
2023-06-06 02:14:16 +0700

    Implement player spaceship controller

---
+++ Assets/Project/Core/Code/Scripts/Gameplay/Controllers/InputConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace SpaceInvaders.Gameplay.Controllers
+{
+    public static class InputConstants
+    {
+        public const string HorizontalAxis = "Horizontal";
+        public const string VerticalAxis = "Vertical";
+        public const string FireButton = "Fire1";
+    }
+}
---
+++ Assets/Project/Core/Code/Scripts/Gameplay/Controllers/PlayerSpaceshipController.cs
@@ -0,0 +1,28 @@
+﻿using SpaceInvaders.Foundation.TinyGameplayFramework;
+using SpaceInvaders.Gameplay.Units;
+using UnityEngine;
+
+namespace SpaceInvaders.Gameplay.Controllers
+{
+    public class PlayerSpaceshipController : GameplayUnitController<Spaceship>
+    {
+        private Vector2 _movementInput;
+
+        protected override void TargetBasedUpdate()
+        {
+            _movementInput = new Vector2(Input.GetAxisRaw(InputConstants.HorizontalAxis), Input.GetAxisRaw(InputConstants.VerticalAxis));
+
+            if (Input.GetButtonDown(InputConstants.FireButton))
+            {
+                Target.Fire();
+            }
+        }
+
+        protected override void TargetBasedFixedUpdate()
+        {
+            if (Mathf.Abs(_movementInput.sqrMagnitude) <= Mathf.Epsilon) return;
+
+            Target.Move(_movementInput);
+        }
+    }
+}
