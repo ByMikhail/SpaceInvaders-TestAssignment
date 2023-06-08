c884569469be4d23d40b50bee5ed4fd3bb44b810
Mikhail Bychkov <mikbychkov@gmail.com>
2023-06-09 02:29:20 +0700

    Implement invader

---
+++ Assets/Project/Core/Code/Scripts/Gameplay/Units/Invader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using SpaceInvaders.Foundation.TinyGameplayFramework;
+using SpaceInvaders.Gameplay.Units.AbilityInterfaces;
+using SpaceInvaders.Gameplay.Units.Factories;
+using UnityEngine;
+
+namespace SpaceInvaders.Gameplay.Units
+{
+    public class Invader : GameplayUnit, IKillable
+    {
+        [SerializeField] private Transform _projectileSpawnPoint;
+        [SerializeField] private ProjectileFactoryBase _projectileFactory;
+
+        public event EventHandler OnKilled;
+
+        public void Fire()
+        {
+            _projectileFactory.Create(tag, _projectileSpawnPoint.position, _projectileSpawnPoint.rotation);
+        }
+
+        public void Kill()
+        {
+            OnKilled?.Invoke(this, EventArgs.Empty);
+            DestroyItself();
+        }
+    }
+}
