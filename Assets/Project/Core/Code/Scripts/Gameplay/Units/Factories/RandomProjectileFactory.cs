18f77130ada0d50a21212a235a28fea1162474ee
Mikhail Bychkov <mikbychkov@gmail.com>
2023-06-09 02:28:59 +0700

    Implement random projectile factory

---
+++ Assets/Project/Core/Code/Scripts/Gameplay/Units/Factories/RandomProjectileFactory.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace SpaceInvaders.Gameplay.Units.Factories
+{
+    [CreateAssetMenu(menuName = "Gameplay/Factories/RandomProjectileFactory", fileName = "RandomProjectileFactory")]
+    public class RandomProjectileFactory : ProjectileFactoryBase
+    {
+        [SerializeField] private Projectile[] _projectilePrefabs;
+
+        public override Projectile Create(string ignoreTag, Vector3 position, Quaternion direction)
+        {
+            int projectilePrefabIndex = Random.Range(0, _projectilePrefabs.Length);
+            var projectilePrefab = _projectilePrefabs[projectilePrefabIndex];
+
+            var projectile = Instantiate(projectilePrefab, position, direction);
+            projectile.Init(ignoreTag);
+
+            return projectile;
+        }
+    }
+}
