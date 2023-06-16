aec5ee0b415deadecc51936225b1406102469381
Mikhail Bychkov <mikbychkov@gmail.com>
2023-06-16 23:20:21 +0700

    Implement InvadersGroupController.cs

---
+++ Assets/Project/Core/Code/Scripts/Gameplay/Controllers/InvadersGroupController.cs
@@ -0,0 +1,24 @@
+﻿using SpaceInvaders.Foundation.TinyGameplayFramework;
+using SpaceInvaders.Gameplay.Components;
+using SpaceInvaders.Gameplay.Units;
+using UnityEngine;
+
+namespace SpaceInvaders.Gameplay.Controllers
+{
+    public class InvadersGroupController : GameplayUnitController<InvadersGroup>
+    {
+        [SerializeField] private float _fireRate = 1f;
+
+        private readonly Timer _firingTimer = new();
+
+        protected override void TargetBasedUpdate()
+        {
+            if (_firingTimer.TimeIsOut)
+            {
+                Target.Fire();
+
+                _firingTimer.Restart(_fireRate);
+            }
+        }
+    }
+}
