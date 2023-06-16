e16d4339826d51a1b623b5e5c1f1289afb90d409
Mikhail Bychkov <mikbychkov@gmail.com>
2023-06-16 23:19:21 +0700

    Implement Timer.cs

---
+++ Assets/Project/Core/Code/Scripts/Gameplay/Components/Timer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace SpaceInvaders.Gameplay.Components
+{
+    public class Timer
+    {
+        public bool TimeIsOut => Time.time >= _nextTimeStamp;
+
+        private float _nextTimeStamp = 0f;
+
+        public void Restart(float duration)
+        {
+            _nextTimeStamp = Time.time + duration;
+        }
+    }
+}
