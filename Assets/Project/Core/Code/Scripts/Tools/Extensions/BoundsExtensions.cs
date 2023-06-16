18098abb58df0d1d218630a82501d1d98b3c0740
Mikhail Bychkov <mikbychkov@gmail.com>
2023-06-16 22:49:44 +0700

    Implement Bounds.ToLocalSpace extension

---
+++ Assets/Project/Core/Code/Scripts/Tools/Extensions/BoundsExtensions.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace SpaceInvaders.Tools.Extensions
+{
+    public static class BoundsExtensions
+    {
+        public static Bounds ToLocalSpace(this Bounds worldSpaceBounds, Transform transform)
+        {
+            return new Bounds(transform.transform.InverseTransformPoint(worldSpaceBounds.center), worldSpaceBounds.size);
+        }
+    }
+}
