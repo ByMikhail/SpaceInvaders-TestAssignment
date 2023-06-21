29c596f5863ea4f2e57d580bfa9692ba236f466f
Mikhail Bychkov <mikbychkov@gmail.com>
2023-06-22 00:27:40 +0700

    Implement PropertyField.cs

---
+++ Assets/Project/GUI/Code/Scripts/Views/Base/PropertyField.cs
@@ -0,0 +1,38 @@
+﻿using SpaceInvaders.Tools.Extensions;
+using TMPro;
+using UnityEngine;
+
+namespace SpaceInvaders.GUI.Code.Scripts.Views.Base
+{
+    public abstract class PropertyField<TValue> : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI _labelText;
+
+        public string Label
+        {
+            get => _label;
+            set => value.AssignTo(ref _label, HandleOnLabelChanged);
+        }
+
+        public TValue Value
+        {
+            get => _value;
+            set => value.AssignTo(ref _value, HandleOnValueChanged);
+        }
+
+        private string _label;
+        private TValue _value;
+
+        protected void Awake()
+        {
+            HandleOnValueChanged();
+        }
+
+        protected virtual void HandleOnLabelChanged()
+        {
+            _labelText.text = _label;
+        }
+
+        protected abstract void HandleOnValueChanged();
+    }
+}
