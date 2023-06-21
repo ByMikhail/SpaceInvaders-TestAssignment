10da679f9fd376673841baffe276e7a7f8cc400a
Mikhail Bychkov <mikbychkov@gmail.com>
2023-06-22 00:27:55 +0700

    Implement PropertyFieldInt.cs

---
+++ Assets/Project/GUI/Code/Scripts/Views/PropertyFieldInt.cs
@@ -0,0 +1,16 @@
+﻿using SpaceInvaders.GUI.Code.Scripts.Views.Base;
+using TMPro;
+using UnityEngine;
+
+namespace SpaceInvaders.GUI.Code.Scripts.Views
+{
+    public class PropertyFieldInt : PropertyField<int>
+    {
+        [SerializeField] private TextMeshProUGUI _valueText;
+
+        protected override void HandleOnValueChanged()
+        {
+            _valueText.text = Value.ToString();
+        }
+    }
+}
