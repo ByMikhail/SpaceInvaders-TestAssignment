0ed6ffb28f0fdc8690ce640d8cff94f9f09b3656
Mikhail Bychkov <mikbychkov@gmail.com>
2023-06-22 00:17:15 +0700

    Implement SceneServiceRegistrar.cs

---
+++ Assets/Project/Core/Code/Scripts/Foundation/ServiceLocator/SceneServiceRegistrar.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpaceInvaders.Foundation.ServiceLocator
+{
+    public abstract class SceneServiceRegistrar : MonoBehaviour
+    {
+        private HashSet<Type> _registeredTypes = new();
+
+        #region Unity lifecycle
+
+        private void Awake()
+        {
+            RegisterServices();
+        }
+
+        private void OnDestroy()
+        {
+            ServiceLocator.Default.Unregister(_registeredTypes);
+        }
+
+        #endregion
+
+        protected void Register<TService>(TService service)
+            where TService : class
+        {
+            ServiceLocator.Default.Register(service);
+            _registeredTypes.Add(typeof(TService));
+        }
+
+        protected abstract void RegisterServices();
+    }
+}
