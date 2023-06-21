8104430a51b2608e734ecbed2cab92348989aa56
Mikhail Bychkov <mikbychkov@gmail.com>
2023-06-22 00:20:21 +0700

    Implement SceneManager

---
+++ Assets/Project/Core/Code/Scripts/Infrastructure/Systems/SceneManagement/SceneManager.cs
@@ -0,0 +1,13 @@
+﻿using UnitySceneManager = UnityEngine.SceneManagement.SceneManager;
+
+namespace SpaceInvaders.Infrastructure.Systems.SceneManagement
+{
+    public class SceneManager
+    {
+        public void ReloadCurrentScene()
+        {
+            var currentScene = UnitySceneManager.GetActiveScene();
+            UnitySceneManager.LoadScene(currentScene.buildIndex);
+        }
+    }
+}
