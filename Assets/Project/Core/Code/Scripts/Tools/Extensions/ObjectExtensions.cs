ce7fc85f35bcd84097b561b17b08adfee3226a08
Mikhail Bychkov <mikbychkov@gmail.com>
2023-06-22 00:22:49 +0700

    Add ObjectExtensions.cs

---
+++ Assets/Project/Core/Code/Scripts/Tools/Extensions/ObjectExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SpaceInvaders.Tools.Extensions
+{
+    public static class ObjectExtensions
+    {
+        public static void AssignTo<T>(this T value, ref T targetField, Action onChanged)
+        {
+            if (EqualityComparer<T>.Default.Equals(targetField, value))
+            {
+                return;
+            }
+
+            targetField = value;
+            onChanged?.Invoke();
+        }
+    }
+}
