124e5a54a11c4d9368a6aac30f734a6212231aa5
Mikhail Bychkov <mikbychkov@gmail.com>
2023-06-22 00:24:11 +0700

    Implement MainSceneServiceRegistrar.cs

---
+++ Assets/Project/Core/Code/Scripts/Infrastructure/ServiceRegistrars/MainSceneServiceRegistrar.cs
@@ -0,0 +1,20 @@
+﻿using SpaceInvaders.Foundation.ServiceLocator;
+using SpaceInvaders.Foundation.TinyGameplayFramework;
+using SpaceInvaders.Gameplay.GameModes;
+using SpaceInvaders.Infrastructure.Systems.SceneManagement;
+using UnityEngine;
+
+namespace SpaceInvaders.Infrastructure.ServiceRegistrars
+{
+    public class MainSceneServiceRegistrar : SceneServiceRegistrar
+    {
+        [SerializeField] private ScoreGameMode _gameMode;
+
+        protected override void RegisterServices()
+        {
+            Register<SceneManager>(new SceneManager());
+            Register<GameModeBase>(_gameMode);
+            Register<ScoreGameMode>(_gameMode);
+        }
+    }
+}
